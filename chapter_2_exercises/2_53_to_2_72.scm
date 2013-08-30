225feb8b93f57cc784d423356753dff4043e7f5a
Mark Curtiss <markcurtiss@gmail.com>
2013-08-30 16:43:45 -0700

    Still have to finish deriviation.  Fun with trees!

--- chapter_2_exercises/2_53_to_2_72.scm
+++ chapter_2_exercises/2_53_to_2_72.scm
@@ -59,8 +59,7 @@
 
 ;; (quote (quote abracadabra))
 
-;; Uh and the (car) of that is clearly gonna be the symbol 'quote'
-
+;; Uh and the (car) of that is clearly gonna be the operator 'quote'
 
 ; 2.56
 ; ========================================================================
@@ -143,3 +142,293 @@
 
 ;; 1 ]=> (deriv '(* x y (+ x 3)) 'x)
 ;; Value 165: (+ (* x y) (* y (+ x 3)))
+
+; 2.58
+; ========================================================================
+;;a.
+(define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+ a2))))
+(define (sum? x)  (and (pair? x) (eq? (cadr x) '+)))
+(define (addend s) (car s))
+(define (augend s) (caddr s))
+
+(define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+(define (product? x) (and (pair? x) (eq? (cadr x) '*)))
+(define (multiplier p) (car p))
+(define (multiplicand p) (caddr p))
+
+;; 1 ]=> (deriv '(x + (3 * (x + (y + 2)))) 'x)
+;; Value: 4
+
+;;b. (deriv '(x + 3 * (x + y + 2)) 'x)
+;;should be 4
+;; we need
+;; 2 error> (augend '(x + y + 2))
+;; to return
+;; Value 170: (y + 2)
+
+; 2.59
+; ========================================================================
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (append set1 (filter (lambda (x) (not (element-of-set? x set1))) set2)))
+
+;; 1 ]=> (union-set '(1 2 3 4) '(2 4 6 8))
+;; Value 179: (1 2 3 4 6 8)
+
+; 2.60
+; ========================================================================
+(define ts1 '(1 1 2 3 4))
+(define ts2 '(2 4 6 8 8))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+;; 1 ]=> (element-of-set? 2 ts1)
+;; Value: #t
+;; 1 ]=> (element-of-set? 8 ts1)
+;; Value: #f
+;; This is the same efficiency.
+
+(define (adjoin-set x set)
+  (cons x set))
+
+;; 1 ]=> (adjoin-set 8 ts1)
+;; Value 180: (8 1 1 2 3 4)
+;; This is now a constant time operation!
+
+(define (union-set set1 set2)
+  (append set1 set2))
+
+;; 1 ]=> (union-set ts1 ts2)
+;; Value 181: (1 1 2 3 4 2 4 6 8 8)
+;; This is O(n) whereas before it was O(n^2)
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1) (intersection-set (cdr set1) set2)))
+	(else (intersection-set (cdr set1) set2))))
+
+;; 1 ]=> (intersection-set ts1 ts2)
+;; Value 188: (2 4)
+;; This is O(n^2) again.
+
+;; This would be good for applications that have to be fast
+;; at adding to sets or union-ing sets together.  This could
+;; have been good for our n-queens representation since we were
+;; constantly adjoining sets.
+
+; 2.61
+; ========================================================================
+(define ts1 '(1 2 3 4))
+(define ts2 '(2 4 6 8))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+(define (adjoin-set x set)
+  (let ((head (car set)))
+  (cond  ((= x head) set)
+	 ((< x head) (cons x set))
+	 (else (cons head (adjoin-set x (cdr set)))))))
+
+;; 1 ]=> (adjoin-set 5 ts2)
+;; Value 205: (2 4 5 6 8)
+
+; 2.62
+; ========================================================================
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+	((null? set2) set1)
+	(else
+	 (let ((x1 (car set1)) (x2 (car set2)))
+	   (cond ((= x1 x2)
+		  (cons x1 (union-set (cdr set1) (cdr set2))))
+		 ((< x1 x2)
+		  (cons x1 (union-set (cdr set1) set2)))
+		 ((> x1 x2)
+		  (cons x2 (union-set set1 (cdr set2)))))))))
+
+;; 1 ]=> (union-set ts1 ts2)
+;; Value 211: (1 2 3 4 6 8)
+
+; 2.63
+; ========================================================================
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+;;     7
+;;    / \
+;;   3   9
+;;  / \   \
+;; 1   5   11
+(define tree1 (make-tree 7
+			 (make-tree 3 (make-tree 1 '() '()) (make-tree 5 '() '()))
+			 (make-tree 9 '() (make-tree 11 '() '()))))
+
+;;   3
+;;  / \
+;; 1   7
+;;    / \
+;;   5   9
+;;        \
+;;         11
+
+(define tree2 (make-tree 3
+			 (make-tree 1 '() '())
+			 (make-tree 7 (make-tree 5 '() '()) (make-tree 9 '() (make-tree 11 '() '())))))
+
+;;     5
+;;    / \
+;;   3   9
+;;  /   / \
+;; 1   7  11
+
+(define tree3 (make-tree 5
+			 (make-tree 3 (make-tree 1 '() '()) '())
+			 (make-tree 9 (make-tree 7 '() '()) (make-tree 11 '() '()))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+;;a.
+;; Yes.
+;; 1 ]=> (tree->list-2 tree2)
+;; Value 227: (1 3 5 7 9 11)
+
+;;b.
+;; No.
+
+; 2.64
+; ========================================================================
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+;;a.
+;; (partial-tree) constructs a binary tree of size n from the elements you pass in.
+;; Since you know that the list is ordered, you can treat the center of the list as the root
+;; node and the left and right halves of the list as the left and right subtrees.
+;; Thus, (partial-tree) divides the elements list in half recursively until it runs out of sublists,
+;; then uses (make-tree) to construct balanced binary trees from these sublists with the middle
+;; element of the list at the root.  So (partial-tree '(1 3 4 7 9 11), 3) would return:
+;;   3
+;;  / \
+;; 1   5
+;; The result of (list->tree '(1 3 5 7 9 11)) is:
+;;      5
+;;     / \
+;;    /   \
+;;   1     9
+;;  / \   / \
+;; ()  3 7  11
+;;b.
+;;Since (partial-tree) visits every element in the tree only once, it has O(n) growth.
+
+; 2.65
+; ========================================================================
+(define tree1 (list->tree '(1 2 3 4)))
+(define tree2 (list->tree '(2 4 6 8)))
+
+(define (union-set-tree tree1 tree2)
+  (let ((list1 (tree->list-1 tree1))
+	(list2 (tree->list-1 tree2)))
+    (list->tree (union-set list1 list2))))
+
+;; 1 ]=> (union-set-tree tree1 tree2)
+;; Value 232: (3 (1 () (2 () ())) (6 (4 () ()) (8 () ())))
+;;     3
+;;    / \
+;;   /   \
+;;  1     6
+;;   \   / \
+;;    2 4   8
+
+(define (intersection-set-tree tree1 tree2)
+  (let ((list1 (tree->list-1 tree1))
+	(list2 (tree->list-1 tree2)))
+    (list->tree (intersection-set list1 list2))))
+
+;; 1 ]=> (intersection-set-tree tree1 tree2)
+;; Value 233: (2 () (4 () ()))
+;;  2
+;;   \
+;;    4
+
+; 2.66
+; ========================================================================
