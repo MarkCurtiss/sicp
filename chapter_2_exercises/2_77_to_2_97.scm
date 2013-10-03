b7c822f020e9635303411f7c70c22f052b67d364
Mark Curtiss <markcurtiss@gmail.com>
2013-10-02 20:56:06 -0700

    2.87 was not too bad once I understood what they were aksing

--- chapter_2_exercises/2_77_to_2_97.scm
+++ chapter_2_exercises/2_77_to_2_97.scm
@@ -972,3 +972,101 @@
 ;; Value 5877: (complex polar (scheme-number . 17.775070323348935) scheme-number . 1.7833631363579512)
 ;; 1 ]=> (div (make-complex-from-real-imag (make-rational 3 8) 1) (make-complex-from-real-imag 14 9))
 ;; Value 6020: (complex polar (scheme-number . .0641699289651586) scheme-number . .6406881766906976)
+
+; 2.87
+; ========================================================================
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
+  ;; representation of terms and term lists
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+	term-list
+	(cons term term-list)))
+  (define (the-empty-termlist) '())
+  (define (first-term term-list) (car term-list))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (empty-termlist? term-list) (null? term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+
+  (define (add-poly p1 p2)
+    (define (add-terms L1 L2)
+      (cond ((empty-termlist? L1) L2)
+	    ((empty-termlist? L2) L1)
+	    (else
+	     (let ((t1 (first-term L1)) (t2 (first-term L2)))
+	       (cond ((> (order t1) (order t2))
+		      (adjoin-term
+		       t1 (add-terms (rest-terms L1) L2)))
+		     ((< (order t1) (order t2))
+		      (adjoin-term
+		       t2 (add-terms L1 (rest-terms L2))))
+		     (else
+		      (adjoin-term
+		       (make-term (order t1)
+				  (add (coeff t1) (coeff t2)))
+		       (add-terms (rest-terms L1)
+				  (rest-terms L2)))))))))
+
+    (if (same-variable? (variable p1) (variable p2))
+	(make-poly (variable p1)
+		   (add-terms (term-list p1)
+			      (term-list p2)))
+	(error "Polys not in same var -- ADD-POLY"
+	       (list p1 p2))))
+
+  (define (mul-poly p1 p2)
+    (define (mul-terms L1 L2)
+      (if (empty-termlist? L1)
+	  (the-empty-termlist)
+	  (add-terms (mul-term-by-all-terms (first-term L1) L2)
+		     (mul-terms (rest-terms L1) L2))))
+    (define (mul-term-by-all-terms t1 L)
+      (if (empty-termlist? L)
+	  (the-empty-termlist)
+	  (let ((t2 (first-term L)))
+	    (adjoin-term
+	     (make-term (+ (order t1) (order t2))
+			(mul (coeff t1) (coeff t2)))
+	     (mul-term-by-all-terms t1 (rest-terms L))))))
+
+    (if (same-variable? (variable p1) (variable p2))
+	(make-poly (variable p1)
+		   (mul-terms (term-list p1)
+			      (term-list p2)))
+	(error "Polys not in same var -- MUL-POLY"
+	       (list p1 p2))))
+
+  ;; HERE IS THE DEFINITION OF =ZERO?
+  (define (=zero?-poly x)
+    (every (lambda (term) (= (coeff term) 0)) (term-list x)))
+  ;; END
+
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  (put '=zero? '(polynomial) =zero?-poly)
+  'done)
+
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
+
+(install-polynomial-package)
+
+;; 1 ]=> (=zero? (make-polynomial 'x (list '(100 0) '(80 0))))
+;; Value: #t
+;; 1 ]=> (=zero? (make-polynomial 'x (list '(100 0) '(80 80))))
+;; Value: #f
