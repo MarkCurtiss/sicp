d30c86e5b2c18a53865f4cff2b6d4cd1735a1e08
Mark Curtiss <markcurtiss@gmail.com>
2014-01-05 13:56:49 -0800

    This was really interesting but the drawing was tedious.

--- chapter_3_exercises/3_9_to_3_11.scm
+++ chapter_3_exercises/3_9_to_3_11.scm
@@ -104,7 +104,7 @@
 ;;      +--+           +-------------------+
 ;;   +---+ |                     ^
 ;;   |  |--|             +-------+----+
-;;   |  | +----+  E2+--->|balance: 100|
+;;   |  | +----+  E2+--->|balance: 50 |
 ;;   |  +--+   |         +------------+
 ;;   |         |               ^
 ;;   |         +---------------+
@@ -128,7 +128,7 @@
 ;;      +--+           +-------------------+        |       E3+--->|initial-amount: 100|
 ;;   +---+ |                     ^                  |              +-------------------+
 ;;   |  |--|             +-------+----+             |                       ^
-;;   |  | +----+  E2+--->|balance: 100|             v                +------+-----+
+;;   |  | +----+  E2+--->|balance: 50 |             v                +------+-----+
 ;;   |  +--+   |         +------------+            +--+       E4+--->|balance: 100|
 ;;   |         |               ^         +----------+ |              +------------+
 ;;   |         +---------------+         |         |--|                   ^
@@ -142,3 +142,180 @@
 ;; in that the double-lambdas cause the resultant code objects to carry
 ;; around references to an environment containing "initial-amount".
 
+; 3.11
+; ========================================================================
+;; (define acc (make-account 50))
+;;
+;;               +------------------+          +--+    params: balance
+;; global-env+-->|      make-account+--------->| +---->body:
+;;               |                  |          |--|      (define (withdraw amount)
+;;     +---------+acc               |<----------+ |        ...
+;;     |         +------------------+          +--+      (define (deposit amount)
+;;     |               ^                                   ...
+;;     |               |                                 (define (dispatch m)
+;;     v            +-----------+                          ...
+;;   +--+    E1+--->|balance: 50|          +--+          dispatch)
+;; +--+ |           |withdraw+------------>| +------+
+;; | |--|           |deposit+------+       |--|     |
+;; | | +----------->|dispatch   |<----------+ |     |
+;; | +--+           +------+----+  |       +--+     |
+;; |                       |  ^ ^  |                |
+;; |                       |  | |  |                v
+;; |                       v  | |  v            params: amount
+;; |                    +--+  | |+--+           body:
+;; |                +----+ |  | || +----+         (if (>= balance amount)
+;; |                |   |--|  | ||--|   v           ...
+;; |                |   | +---+ +-+ |   params: amount
+;; |                |   +--+     +--+   body:
+;; |                |                     (set! balance (+ balance amount))
+;; +>params: m <----+                     balance)
+;;   body:
+;;     ((cond (eq ? m 'withdraw) withdraw)
+;;     ...
+
+;; ((acc 'deposit) 40)
+;;
+;;               +------------------+          +--+    params: balance
+;; global-env+-->|      make-account+--------->| +---->body:
+;;               |                  |          |--|      (define (withdraw amount)
+;;     +---------+acc               |<----------+ |        ...
+;;     |         +------------------+          +--+      (define (deposit amount)
+;;     |               ^                                   ...
+;;     |               |                                 (define (dispatch m)
+;;     v            +-----------+                          ...
+;;   +--+    E1+--->|balance: 50|                        dispatch)
+;; +--+ |           |withdraw   |
+;; | |--|           |deposit<----------------------+
+;; | | +----------->|dispatch<-------+             |
+;; | +--+           +-----------+    |             |
+;; |                                 |             |
+;; |                                 |             |
+;; |                           +-----+-----+       |
+;; |                    E2+--->|m: 'deposit|       |
+;; |                           +-----------+       |
+;; |                            ((eq? m 'deposit) deposit)
+;; |                              ...              |
+;; |                                               |
+;; |                                         +-----+-------+
+;; +>params: m                        E3+--->|amount: 40   |
+;;   body:                                   +-------------+
+;;     ((cond (eq? m 'withdraw) withdraw)     (set! balance (+ balance amount))
+;;       ...                                    ...
+;;
+;; Then:
+;;
+;;               +------------------+          +--+    params: balance
+;; global-env+-->|      make-account+--------->| +---->body:
+;;               |                  |          |--|      (define (withdraw amount)
+;;     +---------+acc               |<----------+ |        ...
+;;     |         +------------------+          +--+      (define (deposit amount)
+;;     |               ^                                   ...
+;;     |               |                                 (define (dispatch m)
+;;     v            +-----------+                          ...
+;;   +--+    E1+--->|balance: 90|                        dispatch)
+;; +--+ |           |withdraw   |
+;; | |--|           |deposit    |
+;; | | +----------->|dispatch   |
+;; | +--+           +-----------+
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; +>params: m
+;;   body:
+;;     ((cond (eq? m 'withdraw) withdraw)
+;;       ...
+
+;; ((acc 'withdraw) 60)
+;;               +------------------+          +--+    params: balance
+;; global-env+-->|      make-account+--------->| +---->body:
+;;               |                  |          |--|      (define (withdraw amount)
+;;     +---------+acc               |<----------+ |        ...
+;;     |         +------------------+          +--+      (define (deposit amount)
+;;     |               ^                                   ...
+;;     |               |                                 (define (dispatch m)
+;;     v            +-----------+                          ...
+;;   +--+    E1+--->|balance: 90|                        dispatch)
+;; +--+ |           |withdraw<----------------------+
+;; | |--|           |deposit    |                   |
+;; | | +----------->|dispatch<-------+              |
+;; | +--+           +-----------+    |              |
+;; |                                 |              |
+;; |                          +------+-----+        |
+;; |                   E2+--->|m: 'withdraw|        |
+;; |                          +------------+        |
+;; |                  ((cond (eq? m 'withdraw) withdraw)
+;; |                    ...                   +-----+----+
+;; |                                   E3+--->|amount: 60|
+;; |                                          +----------+
+;; |                                           (if (>= balance amount)
+;; +>params: m                                   ...
+;;   body:
+;;     ((cond (eq? m 'withdraw) withdraw)
+;;       ...
+;; Then:
+;;               +------------------+          +--+    params: balance
+;; global-env+-->|      make-account+--------->| +---->body:
+;;               |                  |          |--|      (define (withdraw amount)
+;;     +---------+acc               |<----------+ |        ...
+;;     |         +------------------+          +--+      (define (deposit amount)
+;;     |               ^                                   ...
+;;     |               |                                 (define (dispatch m)
+;;     v            +-----------+                          ...
+;;   +--+    E1+--->|balance: 30|                        dispatch)
+;; +--+ |           |withdraw   |
+;; | |--|           |deposit    |
+;; | | +----------->|dispatch   |
+;; | +--+           +-----------+
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; |
+;; +>params: m
+;;   body:
+;;     ((cond (eq? m 'withdraw) withdraw)
+;;       ...
+
+;; (acc)'s local state is kept in an environment E1.
+;;
+;; (define acc2 (make-account 100))
+;;               +------------------+          +--+    params: balance
+;; global-env+-->|      make-account+--------->| +---->body:
+;;               |                  |          |--|      (define (withdraw amount)
+;;     +---------+acc          acc2 |<----------+ |        ...
+;;     |         +----------------+-+          +--+      (define (deposit amount)
+;;     |               ^          |^                       ...
+;;     |               |          |+---------+           (define (dispatch m)
+;;     v            +-----------+ |       +--+--------+    ...
+;;   +--+    E1+--->|balance: 30| |E2+--->|balance:100|  dispatch)
+;; +--+ |           |withdraw   | |       |withdraw   |
+;; | |--|           |deposit    | |       |deposit    |
+;; | | +----------->|dispatch   | |       |dispatch   |
+;; | +--+           +-----------+ |       +-----------+
+;; |                              |            ^
+;; |                              v            |
+;; |                            +--+           |
+;; |                            | +------>params: m
+;; |                            |--|      body:
+;; |                            | +|        ((cond (eq? m 'withdraw) withdraw)
+;; |                            +-|+          ...
+;; |                              |            |
+;; |                              +------------+
+;; +>params: m
+;;   body:
+;;     ((cond (eq? m 'withdraw) withdraw)
+;;       ...
+
+;; The two account objects don't share any state, not even method definitions.
+;; The only thing they have in common is the same parent environment, the
+;; global environment.
