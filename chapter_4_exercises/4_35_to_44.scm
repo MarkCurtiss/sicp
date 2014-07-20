d636a77085c5765c0631d7150b0ae51e7c998d5b
Mark Curtiss <markcurtiss@gmail.com>
2014-07-20 14:03:18 -0700

    Logic puzzles are easy with (amb) and (require)

--- chapter_4_exercises/4_35_to_44.scm
+++ chapter_4_exercises/4_35_to_44.scm
@@ -95,3 +95,157 @@
 ;; Yes Ben's implementation should be more efficient.  He only has to explore
 ;; at max (high - low)^2 possibilities while the previous implementation
 ;; had to explore (high - low)^3 possibilities.
+
+
+; 4.38
+; ========================================================================
+;; (define (distinct? items)
+;;   (cond ((null? items) true)
+;;         ((null? (cdr items)) true)
+;;         ((member (car items) (cdr items)) false)
+;;         (else (distinct? (cdr items)))))
+
+;; (define (multiple-dwelling)
+;;   (let ((baker (amb 1 2 3 4 5))
+;;         (cooper (amb 1 2 3 4 5))
+;;         (fletcher (amb 1 2 3 4 5))
+;;         (miller (amb 1 2 3 4 5))
+;;         (smith (amb 1 2 3 4 5)))
+;;     (require
+;;      (distinct? (list baker cooper fletcher miller smith)))
+;;     (require (not (= baker 5)))
+;;     (require (not (= cooper 1)))
+;;     (require (not (= fletcher 5)))
+;;     (require (not (= fletcher 1)))
+;;     (require (> miller cooper))
+;;     (require (not (= (abs (- fletcher cooper)) 1)))
+;;     (list (list 'baker baker)
+;;           (list 'cooper cooper)
+;;           (list 'fletcher fletcher)
+;;           (list 'miller miller)
+;;           (list 'smith smith))))
+
+;; ;;; Amb-Eval input:
+;; (define (distinct? items)
+;;   (cond ((null? items) true)
+;;         ((null? (cdr items)) true)
+;;         ((member (car items) (cdr items)) false)
+;;         (else (distinct? (cdr items)))))
+
+;; ;;; Starting a new problem
+;; ;;; Amb-Eval value:
+;; ok
+
+;; ;;; Amb-Eval input:
+;; (define (multiple-dwelling)
+;;   (let ((baker (amb 1 2 3 4 5))
+;;         (cooper (amb 1 2 3 4 5))
+;;         (fletcher (amb 1 2 3 4 5))
+;;         (miller (amb 1 2 3 4 5))
+;;         (smith (amb 1 2 3 4 5)))
+;;     (require
+;;      (distinct? (list baker cooper fletcher miller smith)))
+;;     (require (not (= baker 5)))
+;;     (require (not (= cooper 1)))
+;;     (require (not (= fletcher 5)))
+;;     (require (not (= fletcher 1)))
+;;     (require (> miller cooper))
+;;     (require (not (= (abs (- fletcher cooper)) 1)))
+;;     (list (list 'baker baker)
+;;           (list 'cooper cooper)
+;;           (list 'fletcher fletcher)
+;;           (list 'miller miller)
+;;           (list 'smith smith))))
+
+;; ;;; Starting a new problem
+;; ;;; Amb-Eval value:
+;; ok
+
+;; ;;; Amb-Eval input:
+;; (multiple-dwelling)
+
+;; ;;; Starting a new problem
+;; ;;; Amb-Eval value:
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 3) (smith 5))
+
+;; ;;; Amb-Eval input:
+;; try-again
+
+;; ;;; Amb-Eval value:
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 5) (smith 3))
+
+;; ;;; Amb-Eval input:
+;; try-again
+
+;; ;;; Amb-Eval value:
+;; ((baker 1) (cooper 4) (fletcher 2) (miller 5) (smith 3))
+
+;; ;;; Amb-Eval input:
+;; try-again
+
+;; ;;; Amb-Eval value:
+;; ((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+
+;; ;;; Amb-Eval input:
+;; try-again
+
+;; ;;; Amb-Eval value:
+;; ((baker 3) (cooper 4) (fletcher 2) (miller 5) (smith 1))
+
+;; ;;; Amb-Eval input:
+;; try-again
+
+;; ;;; There are no more values of
+;; (multiple-dwelling)
+
+;; There are 5 solutions to the modified version of (multiple-dwellings).
+
+; 4.39
+; ========================================================================
+;; The order of the restrictions doesn't affect the answer.  It does
+;; affect the runtime of the program.  If you put more restrictive checks
+;; before less restrictive ones you should be able to cut down on your
+;; problem search space.
+
+;;; Amb-Eval input:
+;; (define (multiple-dwelling)
+;;   (let ((baker (amb 1 2 3 4 5))
+;;         (cooper (amb 1 2 3 4 5))
+;;         (fletcher (amb 1 2 3 4 5))
+;;         (miller (amb 1 2 3 4 5))
+;;         (smith (amb 1 2 3 4 5)))
+;;     (require
+;;      (distinct? (list baker cooper fletcher miller smith)))
+;;     (require (> miller cooper))
+;;     (require (not (= (abs (- smith fletcher)) 1)))
+;;     (require (not (= (abs (- fletcher cooper)) 1)))
+;;     (require (not (= baker 5)))
+;;     (require (not (= cooper 1)))
+;;     (require (not (= fletcher 5)))
+;;     (require (not (= fletcher 1)))
+;;     (list (list 'baker baker)
+;;           (list 'cooper cooper)
+;;           (list 'fletcher fletcher)
+;;           (list 'miller miller)
+;;           (list 'smith smith))))
+
+;; ;;; Starting a new problem
+;; ;;; Amb-Eval value:
+;; ok
+
+;; ;;; Amb-Eval input:
+;; (multiple-dwelling)
+
+;; ;;; Starting a new problem
+;; ;;; Amb-Eval value:
+;; ((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+
+;; ;;; Amb-Eval input:
+;; try-again
+
+;; ;;; There are no more values of
+;; (multiple-dwelling)
+
+;; ;; However on my comically fast computer this doesn't seem to run any
+;; ;; 'slower' or 'faster' than the original.
+
