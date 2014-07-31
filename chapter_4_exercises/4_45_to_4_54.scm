630b7c0bdc0e4de3704bb0fe1b513123e8e81ad6
Mark Curtiss <markcurtiss@gmail.com>
2014-07-30 20:45:31 -0700

    Man will I ever get any of these to actually run??

--- chapter_4_exercises/4_45_to_4_54.scm
+++ chapter_4_exercises/4_45_to_4_54.scm
@@ -403,3 +403,56 @@ try-again
         ((application? exp) (analyze-application exp))
         (else
          (error "Unknown expression type -- ANALYZE" exp))))
+
+; 4.51
+; ========================================================================
+(define (permanent-assignment? exp)
+  (tagged-list? exp 'permanent-set!))
+
+(define (analyze-permanent-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+	       (set-variable-value! var val env)
+	       (succeed 'ok fail2))
+	     fail))))
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+	((permanent-assignment? exp) (analyze-permanent-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((let? exp) (analyze (let->combination exp)))
+        ((amb? exp) (analyze-amb exp))
+	((ramb? exp) (analyze-ramb exp))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+(define count 0)
+(define (an-element-of items)
+  (require (not (null? items)))
+  (amb (car items) (an-element-of (cdr items))))
+(define (require p) (if (not p) (amb)))
+
+(let ((x (an-element-of '(a b c)))
+      (y (an-element-of '(a b c))))
+  (set! count (+ count 1))
+  (require (not (eq? x y)))
+  (list x y count))
+
+;;How do I get the evaluator to reload the definition of eval?
+;;It doesn't know about permanent-set! even after I reload
+;;analyze.
+
+;;Anyways if we didn't use permanent-set! the results would be
+;;(a b 1) (a c 1)
