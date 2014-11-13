18024ebf4662347632e6ecae6a4240460f386979
Mark Curtiss <markcurtiss@gmail.com>
2014-11-13 09:44:49 -0800

    Done with 5.32

--- chapter_5_exercises/5_31_to_5_52.scm
+++ chapter_5_exercises/5_31_to_5_52.scm
@@ -16,3 +16,37 @@
 
 (f (g 'x) 'y)
 ;; You could optimize out 1, 2
+
+; 5.32
+; ========================================================================
+;; a.
+
+;; Put the following in ch5-eceval.scm
+
+;; +(define (application-of-symbol? exp)
+;; +  (and (application? exp)
+;; +       (symbol? (operator exp))))
+
+;;     (list 'application? application?)
+;; +   (list 'application-of-symbol? application-of-symbol?)
+
+;;    (branch (label ev-begin))
+;; +  (test (op application-of-symbol?) (reg exp))
+;; +  (branch (label ev-symbol-application))
+
+;; +ev-symbol-application
+;; +  (save continue)
+;; +  (assign unev (op operands) (reg exp))
+;; +  (save unev)
+;; +  (assign exp (op operator) (reg exp))
+;; +  (assign continue (label ev-appl-did-operator))
+;; +  (goto (label eval-dispatch))
+
+;; Notice that ev-symbol-application doesn't save (env) before jumping
+;; to eval-dispatch.
+
+;; b.
+;; I am against this idea!  It would make the evaluator much more complex.
+;; Also you'd have scads of code in the evaluator that was related solely
+;; to performance optimizations rather than the syntax and semantics of
+;; your language.
